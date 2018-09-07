69a2479e57b95c2fcda44c992e523fc9cc8de90a
s3vi26 <s3vi26@outlook.com>
2018-09-06 17:21:32 -0700

    1st

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { sayings: "" };
+  }
+  
+  update(e) {
+    this.setState({ sayings: this.refs.btalks.value });
+  }
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        Bob says <input type="text" ref="btalks" onChange={this.update.bind(this)} />
       </div>
     );
   }
